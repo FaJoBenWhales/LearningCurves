c3f6fc5655b9160d0ecb4601841ec40cf8831a02
Jochen Sautter <JochenSautter@t-online.de>
2018-02-28 01:36:30 +0100

    lots of cleanup and debugging

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 # Deep learning Theory final project
 import numpy as np
 import time
+import keras.backend as K
 from keras import optimizers
 from keras.models import Model, Sequential
 from keras.layers import Input, Dense, Dropout, LSTM, concatenate
 from keras.regularizers import l1_l2
-from keras.callbacks import EarlyStopping, LearningRateScheduler
+from keras.callbacks import EarlyStopping, LearningRateScheduler, Callback
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score, KFold
@@ -14,42 +15,47 @@ import xgboost as xgb
 import tools as t
 
 
-def mlp():
+def mlp(lr=None, dropout=False, L1L2=False):
     
-    if t.mlp_cfg['L1L2']:
+    # print("create mlp using learning rate:", lr)
+    
+    if L1L2==True:
         kernel_regularizer=l1_l2(l1=0.01, l2=0.01)
-        print("L1L2 regularisation")
+        print("create mlp using L1L2 regularisation")
     else: 
         kernel_regularizer = None
 
     model = Sequential()
     model.add(Dense(64, input_dim=5, kernel_initializer='normal', 
                     kernel_regularizer=kernel_regularizer, activation='relu'))
-    if t.mlp_cfg['dropout']:
-        model.add(Dropout(0.7))
+    if dropout==True:
+        model.add(Dropout(0.2))
+        print("create mlp using Dropout")
     model.add(Dense(64, kernel_initializer='normal', 
                     kernel_regularizer=kernel_regularizer, activation='relu'))
-    if t.mlp_cfg['dropout']:
-        model.add(Dropout(0.7))
+    if dropout==True:
+        model.add(Dropout(0.2))
     model.add(Dense(1, kernel_initializer='normal'))
     
-    opt = optimizers.SGD(lr=t.mlp_cfg['lr'])
-    # opt = optimizers.Adam(lr=t.mlp_cfg['lr'], beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)    
+    opt = optimizers.SGD(lr=lr)
+    
     model.compile(loss='mean_squared_error', optimizer=opt)  # no adaptive learning rate (--> later compare to exponential)
     
     return model
 
 
-def lstm():
-    data_dim = t.mlp_cfg['input_dim']      # features of input data
-    # timesteps = 10    # 
+def lstm(input_dim=1):
+    # usually 1, but changed to 1+5=6 when loading data by load_lstm_data_concat_cfg() 
+    # --> adding configs to each data point of learning curves
+    print("build lstm with input_dim:", input_dim)
+    data_dim = input_dim      # features of input data
+    
     output_dim = 1
     
     model = Sequential()
     model.add(LSTM(64, return_sequences=True,
                    # input shape = (timesteps, data_dim) - but timesteps can be None for flexible use
                    input_shape=(None, data_dim)))  # returns a sequence of vectors of dimension 32
-    # model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
     model.add(LSTM(64))  # return a single vector of dimension 32
     # model.add(Dense(10, activation='softmax'))
     model.add(Dense(64))
@@ -64,11 +70,8 @@ def lstm():
 
 def multi_lstm():
     
-    data_dim = t.mlp_cfg['input_dim']      # features of input data, variable is set in load_lstm_data
-    # timesteps = 10    # 
+    data_dim = 1
     output_dim = 1
-    
-    # model = Sequential()
     
     lcs_input = Input(shape=(None, data_dim), name='lcs_input')
 
@@ -89,153 +92,112 @@ def multi_lstm():
                   metrics=['mse'])
     return model
 
-'''
-ToDo: pass learning rate at creation of model
-def train_mlp(model, cfgs, Y, cfg, split, epochs):
-    cfgs_train, cfgs_val = cfgs[:split], cfgs[split:]
-    y_train, y_val = Y[:split], Y[split:]
-    print(cfgs_train.shape, cfgs_val.shape, y_train.shape, y_val.shape)    
 
-    model.fit(cfgs_train, y_train,
+# ToDo: pass learning rate at creation of model
+def train_mlp(model, configs, Y, cfg, split, epochs):
+    
+    configs_train, configs_val = configs[:split], configs[split:]
+    y_train, y_val = Y[:split], Y[split:]
+    # print(configs_train.shape, configs_val.shape, y_train.shape, y_val.shape)    
+
+    model.fit(configs_train, y_train,
               batch_size=cfg['batch_size'], 
               epochs=epochs,
-              validation_data=(cfgs_val, y_val))
-'''
+              validation_data=(configs_val, y_val))
+
+
+def eval_mlp(model, configs, Y, split, batch_size):
+    
+    configs_train, configs_val = configs[:split], configs[split:]
+    y_train, y_val = Y[:split], Y[split:]             
+    
+    mse = model.evaluate(configs_val, y_val, batch_size=batch_size)
+    print("mse: ", mse)
+    
+    return mse        
 
 
 def train_lstm(model, lcs, Y, split, batch_size, epochs):
+   
     x_train, x_val = lcs[:split], lcs[split:]
     y_train, y_val = Y[:split], Y[split:]
-    print(x_train.shape, x_val.shape, y_train.shape, y_val.shape)    
 
     model.fit(x_train, y_train,
               batch_size=batch_size, epochs=epochs,
               validation_data=(x_val, y_val))
 
-    
-def train_multi_lstm_old(model, cfgs, lcs, Y, split, batch_size, epochs):
-    x_train, x_val = lcs[:split], lcs[split:]
-    y_train, y_val = Y[:split], Y[split:]
-    cfgs_train, cfgs_val = cfgs[:split], cfgs[split:]
-    print(x_train.shape, x_val.shape, cfgs_train.shape, cfgs_val.shape, y_train.shape, y_val.shape)    
 
-    model.fit([cfgs_train, x_train], y_train,
-              batch_size=batch_size, epochs=epochs,
-              validation_data=([cfgs_val, x_val], y_val))
-
-    
-def train_multi_lstm(model, x_cfgs, Y, split, batch_size, epochs):
-    x_cfgs_train, x_cfgs_val = [], []
-
-    # x_cfgs_train = list(len(x_cfgs))
-    for i in range(len(x_cfgs)):
-        x_cfgs_train.append(x_cfgs[i][:split])
-        x_cfgs_val.append(x_cfgs[i][split:])
-        
-        # x_cfgs_train[i], x_cfgs_val[i] = x_cfgs[i][:split], x_cfgs[i][split:]
-    
-    # x_train, x_val = lcs[:split], lcs[split:]
-    y_train, y_val = Y[:split], Y[split:]
-    # cfgs_train, cfgs_val = cfgs[:split], cfgs[split:]
-    print(len(x_cfgs_train), len(x_cfgs_val), y_train.shape, y_val.shape)    
-    print("cfgs_train/val:", x_cfgs_train[0].shape, x_cfgs_val[0].shape, 
-          "lcs_train/val:", x_cfgs_train[1].shape, x_cfgs_val[1].shape, 
-          "lcs_end train/val:", y_train.shape, y_val.shape)    
-    # print("x_cfgs_train/val:", x_cfgs_train.shape)#, x_cfgs_val.shape)
-    
-    # x_arr_train = np.array(x_cfgs_train)
-    # x_arr_val = np.array(x_cfgs_val)
-    
-    model.fit(x_cfgs_train, y_train,
-              batch_size=batch_size, epochs=epochs,
-              validation_data=(x_cfgs_val, y_val))
-
-# check if model.fit works with array of tuples instead of tuple of arrays... --> does not work    
-def train_multi_lstm_test(model, x_cfgs, Y, split, batch_size, epochs):
-    x_cfgs_train, x_cfgs_val = [], []
-    
-    for i in range(split):
-        x_cfgs_train.append((x_cfgs[0][i],x_cfgs[1][i]))
-    print("len(x_cfgs)", len(x_cfgs[0]))
-    for i in range(split,len(x_cfgs[0])):
-        x_cfgs_val.append((x_cfgs[0][i],x_cfgs[1][i]))
-    
-    '''
-    # x_cfgs_train = list(len(x_cfgs))
-    for i in range(len(x_cfgs)):
-        x_cfgs_train.append(x_cfgs[i][:split])
-        x_cfgs_val.append(x_cfgs[i][split:])
-        
-        # x_cfgs_train[i], x_cfgs_val[i] = x_cfgs[i][:split], x_cfgs[i][split:]
-    '''
-    
-    # x_train, x_val = lcs[:split], lcs[split:]
-    y_train, y_val = Y[:split], Y[split:]
-    # cfgs_train, cfgs_val = cfgs[:split], cfgs[split:]
-    print(len(x_cfgs_train), len(x_cfgs_val), y_train.shape, y_val.shape)    
-    print("cfgs_train/val:", len(x_cfgs_train), len(x_cfgs_val)) 
-    print("lcs_train/val[7]:", len(x_cfgs_train[7]), len(x_cfgs_val[7])) 
-    print("lcs_train/val[7][0]:", len(x_cfgs_train[7][0]), len(x_cfgs_val[7][0]))
-    print("lcs_train/val[7][0].shape:", x_cfgs_train[7][0].shape, x_cfgs_val[7][0].shape)    
-    print("lcs_train/val[7][1].shape:", x_cfgs_train[7][1].shape, x_cfgs_val[7][1].shape)     
-    print("lcs_end train/val:", y_train.shape, y_val.shape)   
-    # print("x_cfgs_train/val:", x_cfgs_train.shape)#, x_cfgs_val.shape)
-    
-    # x_arr_train = np.array(x_cfgs_train)
-    # x_arr_val = np.array(x_cfgs_val)
-    
-    model.fit(x_cfgs_train, y_train,
-              batch_size=batch_size, epochs=epochs,
-              validation_data=(x_cfgs_val, y_val))    
-    
-    
 def eval_lstm(model, lcs, Y, split, batch_size):
+    
     x_train, x_val = lcs[:split], lcs[split:]
     y_train, y_val = Y[:split], Y[split:]              
-    print("x_train.shape",x_train.shape)
-    print("x_train[0]",x_train[0])
     
-    score, mse = model.evaluate(x_val, y_val,
-                                batch_size=5)
-    print("x_train.shape",x_train.shape)
+    score, mse = model.evaluate(x_val, y_val, batch_size=batch_size)
     print("mse: ", mse)
     
-    return mse
-
-
-def eval_multi_lstm(model, cfgs, lcs, Y, split, batch_size):
+    return mse    
+    
+    
+def train_multi_lstm(model, configs, lcs, Y, split, batch_size, epochs):
+    # lcs, Y = t.shape_lstm_data(lcs, Y, timesteps)
+    
     x_train, x_val = lcs[:split], lcs[split:]
     y_train, y_val = Y[:split], Y[split:]
-    cfgs_train, cfgs_val = cfgs[:split], cfgs[split:]    
-    print("x_val.shape",x_train.shape)
-    print("x_val[0]",x_train[0])
-    print("cfgs_val.shape",x_train.shape)
-    print("cfgs_val[0]",x_train[0])
-    
-    score, mse = model.evaluate([x_val, cfgs_val], y_val,
+    configs_train, configs_val = configs[:split], configs[split:]
+    # print(x_train.shape, x_val.shape, configs_train.shape, configs_val.shape, y_train.shape, y_val.shape)    
+
+    model.fit([configs_train, x_train], y_train,
+              batch_size=batch_size, epochs=epochs,
+              validation_data=([configs_val, x_val], y_val))
+
+
+def eval_multi_lstm(model, configs, lcs, Y, split, batch_size):
+    x_train, x_val = lcs[:split], lcs[split:]
+    y_train, y_val = Y[:split], Y[split:]
+    configs_train, configs_val = configs[:split], configs[split:]    
+   
+    score, mse = model.evaluate([configs_val, x_val], y_val,
                                 batch_size=5)
     
     return mse
 
-'''
-def forecast_lstm(model, batch_size, lcs):
-    lcs = lcs.reshape(1, 1, len(lcs))
-    yhat = model.predict(lcs, batch_size=batch_size)
-    return yhat[0,0]
-'''
 
-
-def exp_decay(epoch):
-    initial_lrate = t.mlp_cfg['lr']
-    k = t.mlp_cfg['k_exp']
+def exp_decay(epoch, lr, k_exp=1):
+    initial_lrate = lr    
+    # print("exp decay with k = ", k_exp)
+    k = k_exp  
     lrate = initial_lrate * np.exp(-k*epoch)
-    if (epoch % 50 == 0):
+    if (epoch % 100 == 0):
         print("new lr: ", lrate)
     return lrate
 
 
+# copied from https://github.com/keras-team/keras/blob/master/keras/callbacks.py#L591 
+# and enhanced in order to facilitate passing parameter k_exp to schedule function exp_decay
+class MyLearningRateScheduler(Callback):
 
-def get_estimator(model_type, X = None, epochs = 20, cfg = {}):
+    def __init__(self, schedule, init_lr, k_exp=0, verbose=0):
+        super(MyLearningRateScheduler, self).__init__()
+        self.schedule = schedule
+        self.init_lr = init_lr        
+        self.k_exp = k_exp     
+        self.verbose = verbose
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if not hasattr(self.model.optimizer, 'lr'):
+            raise ValueError('Optimizer must have a "lr" attribute.')
+        lr = float(K.get_value(self.model.optimizer.lr))
+        lr = self.schedule(epoch, lr=self.init_lr, k_exp=self.k_exp)
+
+        if not isinstance(lr, (float, np.float32, np.float64)):
+            raise ValueError('The output of the "schedule" function '
+                             'should be float.')
+        K.set_value(self.model.optimizer.lr, lr)
+        if self.verbose > 0:
+            print('\nEpoch %05d: LearningRateScheduler reducing learning rate to %s.' % (epoch + 1, lr))
+
+
+def get_estimator(model_type, X = None, epochs = 20, cfg = {}, dropout=False, L1L2=False):
     # print("get estimator type: ", model_type)
 
     if model_type == 'ridge':
@@ -257,20 +219,15 @@ def get_estimator(model_type, X = None, epochs = 20, cfg = {}):
                                      max_depth = cfg['maxdepth'])
         
     elif model_type == 'mlp':     
-        t.mlp_cfg['lr'] = cfg['lr']   # pass parameters to fn "simple_mlp" via global variable t.mlp_cfg
-        # t.mlp_cfg['input_dim'] = X.shape[1]
-        estimator = KerasRegressor(build_fn=mlp, epochs=epochs, 
+        estimator = KerasRegressor(build_fn=mlp, lr = cfg['lr'], dropout=dropout, L1L2=L1L2, epochs=epochs, 
                                    batch_size=cfg['batch_size'], verbose=0)
         
     elif model_type == 'lstm':
-        # t.mlp_cfg['lr'] = cfg['lr']   # pass parameters to fn "simple_mlp" via global variable t.mlp_cfg
-        # t.mlp_cfg['input_dim'] = X.shape[1]
-        estimator = KerasRegressor(build_fn=lstm, epochs=epochs, 
+        print("x_shape[1]",X.shape[1])
+        estimator = KerasRegressor(build_fn=lstm, input_dim = X.shape[1], epochs=epochs, 
                                    batch_size=cfg['batch_size'], verbose=1)
         
     elif model_type == 'multi_lstm':
-        # t.mlp_cfg['lr'] = cfg['lr']   # pass parameters to fn "simple_mlp" via global variable t.mlp_cfg
-        # t.mlp_cfg['input_dim'] = X.shape[1]
         estimator = KerasRegressor(build_fn=multi_lstm, epochs=epochs, 
                                    batch_size=cfg['batch_size'], verbose=1)
               
@@ -282,41 +239,31 @@ def get_estimator(model_type, X = None, epochs = 20, cfg = {}):
 
 
 # create and train model and evaluate by cross validation
-def eval_cv(model_type, X, Y, cfg = {}, epochs=0, splits = 3):
-    
-    # metrics = loss(self.monitor, ','.join(list(logs.keys())))
-    # metrics = logs.keys()
-    
-    # print(metrics)
+def eval_cv(model_type, X, Y, cfg = {}, epochs=0, splits = 3, 
+            lr_exp_decay=False, earlystop=True, dropout=False, L1L2=False):
     
     fit_params = {}
-    
     if model_type == 'mlp':
         callbacks = []
-        if t.mlp_cfg['earlystop']:
+        if earlystop==True:
             callbacks.append(EarlyStopping(monitor='loss', min_delta=0.0001, patience=100, verbose=1, mode='auto'))
-            # fit_params = {'callbacks': [EarlyStopping(monitor='loss', min_delta=0.0001, patience=100, verbose=1, mode='auto')]}
             print("evaluating with early stopping")
             
-        if t.mlp_cfg['lr_exp_decay']:
-            t.mlp_cfg['lr'] = cfg['lr']   # pass parameters to fn "simple_mlp" via global variable t.mlp_cfg
-            t.mlp_cfg['k_exp'] = cfg['k_exp']   
-            lrate = LearningRateScheduler(exp_decay)
+        if lr_exp_decay==True:
+            lrate = MyLearningRateScheduler(exp_decay, init_lr = cfg['lr'], k_exp=cfg['k_exp'])
             callbacks.append(lrate)
             print("evaluating with exponential decay")
 
         fit_params = {'callbacks': callbacks}
     
-    estimator = get_estimator(model_type, X, epochs, cfg)
+    estimator = get_estimator(model_type, X, epochs, cfg, dropout=dropout, L1L2=L1L2)
 
     kfold = KFold(n_splits=splits, random_state=t.seed)
-    # enforce MSE as scoring, to get comparable results for different models /  cross_validate() more flexible
+    # enforce MSE as scoring, to get comparable results for different models 
     print("call cross_val_score {} epochs, with config {}".format(epochs,cfg))
-    #print("X.shape",X.shape)
-    #print("X",X[0])    
     results = cross_val_score(estimator, X, Y, cv=kfold, scoring='neg_mean_squared_error', verbose=1,
                               fit_params=fit_params)
     print("MSE {}: mean *** {:.5f} *** std: {:.4f}".format(model_type, -results.mean(), results.std()))
-    # print("MSE of all Folds: {}".format(np.round(-results,5)))
+    
     return results
 
